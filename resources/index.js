83be65fb06f415aa0b8d72671e2f5d5f1c6bd0d2
justpekka <ariefkhrisna631@gmail.com>
2021-11-24 14:21:18 +0700

    70%)

--- resources/index.js
+++ resources/index.js
@@ -1,10 +1,15 @@
 // <button class="btn style-btn active" id="menu-hair" data-accessories-list="hair" data-toggle="collapse">Hair</button>
 
 const alpacaImages = Array.from(document.querySelector('#main-image').children)
-const accessoriesList = document.querySelector('#accessories-list')
-const accessoriesStyle = document.querySelector('#style-accessories')
+const accessoriesList = document.querySelector('#accessories-buttons')
+const accessoriesStyle = document.querySelector('#style-buttons')
 
 // let button;
 // alpacaImages.forEach(i => {
 //   console.log(i.getAttribute('data-name'))
 // })
+
+const para = document.createElement("button");
+const node = document.createTextNode("Hair");
+para.appendChild(node);
+accessoriesList.appendChild(para);
